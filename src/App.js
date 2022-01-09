07543908daf9bc418ed549c0f71820876a37a3cf
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2022-01-09 23:51:32 +0700

    change file app js to import component sample

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { UserProvider } from "./context/User";
+import UserAuth from "./UserAuth";
 
 const App = () => {
-  return <div>Simple App</div>;
+  return (
+    <div style={{ padding: "20px" }}>
+      <UserProvider>
+        <UserAuth />
+      </UserProvider>
+    </div>
+  );
 };
 
 export default App;
